91f800cfdf19f640b157a93102db2ad162a19c2e
Raines Demint <raines@qaladder.com>
2022-06-01 21:11:24 -0700

    Solved bugs RDBLOG-25 & RDBLOG-26 for twitter showing below author and post layout.

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -68,14 +68,14 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
+                        <dt className="sr-only">LinkedIn</dt>
                         <dd>
-                          {author.twitter && (
+                          {author.linkedin && (
                             <Link
-                              href={author.twitter}
+                              href={author.linkedin}
                               className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                             >
-                              {author.twitter.replace('https://twitter.com/', '@')}
+                              Contact
                             </Link>
                           )}
                         </dd>
