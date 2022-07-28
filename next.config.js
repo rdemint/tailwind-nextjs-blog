8b85665386dfde56e237d73105d8f960e93175b9
Raines Demint <raines@qaladder.com>
2022-07-27 19:54:14 -0700

    Reverting to bf7ad8d

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://www.google-analytics.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
-  connect-src https://www.google-analytics.com www.google-analytics.com https://stats.g.doubleclick.net;
+  connect-src *;
   font-src 'self';
   frame-src giscus.app
 `
