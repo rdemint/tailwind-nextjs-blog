319f511303b2733d1abe913b2d646b4a13199d9a
Raines Demint <raines@qaladder.com>
2022-10-07 12:44:59 -0700

    Prototype of the docs section with SidebarLayout.

---
+++ data/docs/docsNavList.js
@@ -0,0 +1,28 @@
+export const DocsNavList = [
+  {
+    name: 'Articles',
+    children: [
+      {
+        name: 'Specification documents',
+        slug: '/docs/specification-document-purpose',
+      },
+      {
+        name: 'Change control',
+        slug: '/docs/change-control-foundations',
+      },
+    ],
+  },
+  {
+    name: 'Concepts',
+    children: [
+      {
+        name: 'risk based approach',
+        slug: 'risk-based-approach',
+      },
+      {
+        name: 'least burdensome approach',
+        slug: 'least-burdensome-approach',
+      },
+    ],
+  },
+]
