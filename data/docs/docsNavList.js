7797477034e0a4de4f0ada72c55f95ff4662c346
Raines Demint <raines@qaladder.com>
2022-10-09 06:15:26 -0700

    updated to have subsections in docsNavList

--- data/docs/docsNavList.js
+++ data/docs/docsNavList.js
@@ -3,25 +3,36 @@ export const DocsNavList = [
     name: 'Articles',
     children: [
       {
-        name: 'Specification documents',
-        slug: '/docs/specification-document-purpose',
+        name: 'Change Control',
+        key: 'change-control',
+        children: [
+          {
+            name: 'Specification documents',
+            slug: '/docs/specification-document-purpose',
+          },
+          {
+            name: 'Change control',
+            slug: '/docs/change-control-foundations',
+          },
+        ],
       },
       {
-        name: 'Change control',
-        slug: '/docs/change-control-foundations',
-      },
-    ],
-  },
-  {
-    name: 'Concepts',
-    children: [
-      {
-        name: 'risk based approach',
-        slug: 'risk-based-approach',
-      },
-      {
-        name: 'least burdensome approach',
-        slug: 'least-burdensome-approach',
+        name: 'Design Control',
+        key: 'design-control',
+        children: [
+          {
+            name: 'Design planning',
+            slug: '/docs/design-control-planning',
+          },
+          {
+            name: 'Design inputs',
+            slug: '/docs/design-inputs',
+          },
+          {
+            name: 'Design outputs',
+            slug: '/docs/design-outputs',
+          },
+        ],
       },
     ],
   },
