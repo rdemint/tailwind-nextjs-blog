7ff33524528b335f51fa970ef5028fd09846b504
Raines Demint <raines@qaladder.com>
2022-10-17 11:27:57 -0700

    Few styling updates.

--- components/docs/ActionItem.js
+++ components/docs/ActionItem.js
@@ -1,12 +1,12 @@
 export default function ActionItem({ children }) {
   return (
-    <div className="bg-yellow-500">
+    <div>
       <form>
         <input
           type="checkbox"
           className="border-2 border-rose-500 shadow shadow-rose-100 checked:bg-rose-500 hover:scale-105 focus:text-rose-500 focus:ring-rose-500"
         />
-        <label className="pl-2">{children}</label>
+        <label className="mt-4 p-4">{children}</label>
       </form>
     </div>
   )
--- components/docs/RegLangBox.js
+++
@@ -1,3 +0,0 @@
-export default function RegLangBox({ children }) {
-  return <div className="rounded-lg border-gray-200 bg-gray-50 p-4">{children}</div>
-}
