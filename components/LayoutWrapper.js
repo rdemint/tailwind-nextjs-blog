2ec530ca620fe5b05e1f7d60f47cfa7cd8983639
Raines Demint <raines@qaladder.com>
2022-10-19 10:49:17 -0700

    Revert "extracted Header and fixed some spacing issues."
    This reverts commit 157ccd41e9c7b157170726d78283fc50bab6d9cc.

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,15 +1,52 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
 import SectionContainer from './SectionContainer'
 import Footer from '@/components/Footer'
-import Header from '@/components/Header'
+import MobileNav from '@/components/MobileNav'
+import ThemeSwitch from '@/components/ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
+    <div className="flex h-screen flex-col justify-between">
       <SectionContainer>
-        <main className="mb-auto">{children}</main>
-        <Footer />
+        <header className="flex items-center justify-between py-10">
+          <div>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="mr-3">
+                  <Image src={siteMetadata.siteLogo} height="75" width="464" />
+                </div>
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="px-2 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </header>
       </SectionContainer>
+      <main className="mb-auto">{children}</main>
+      <Footer />
     </div>
   )
 }
