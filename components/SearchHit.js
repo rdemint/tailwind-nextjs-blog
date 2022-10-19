2ec81ad50633383e36d78ffcf10696a8b8d56809
Raines Demint <raines@qaladder.com>
2022-10-19 16:37:00 -0700

    Algolia page now has a Carded Search Model.  Need to extract out into a separate component.

--- components/SearchHit.js
+++ components/SearchHit.js
@@ -2,12 +2,12 @@ import { Highlight } from 'react-instantsearch-hooks-web'
 
 export default function SearchHit({ hit }) {
   return (
-    <article className="spacing-x-4 flex">
-      <h1>
+    <article className="m-2 border border-gray-200 p-2">
+      <h1 className="font-bold">
         <Highlight attribute="title" hit={hit} />
       </h1>
-      <p>{hit.summary}</p>
-      <p>
+      <p className="text-sm">{hit.summary}</p>
+      <p className="text-xs tracking-tight">
         <Highlight attribute="tags" hit={hit} />
       </p>
     </article>
