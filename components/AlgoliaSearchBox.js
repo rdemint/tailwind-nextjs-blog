044f8ac7b187b7106e32bc2850328f4d7a11dc33
Raines Demint <raines@qaladder.com>
2022-10-19 16:09:22 -0700

    Implemented SearchModal

--- components/AlgoliaSearchBox.js
+++ components/AlgoliaSearchBox.js
@@ -1,12 +1,11 @@
 import { SearchBox } from 'react-instantsearch-hooks-web'
-import SubmitIcon from '@/components/SubmitIcon'
 
 export default function AlgoliaSearchBox() {
   return (
     <SearchBox
       placeholder="Search"
       classNames={{
-        root: 'flex flex-grow',
+        root: 'flex',
         input:
           'rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500',
         submitIcon: 'hidden',
--- components/SubmitIcon.js
+++
@@ -1,3 +0,0 @@
-export default function SubmitIcon({ classnames }) {
-  return <div className={classnames}>Submit</div>
-}
